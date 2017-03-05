5d5c3c8f7584acdddf4f5ba5c34a802753066cb4
gaofeng <287823837@qq.com>
2017-03-05 13:08:04 +0800

    Initial commit

---
+++ demo/src/main/java/TestDemo.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by XSOOY on 2017/3/4.
+ */
+@Controller
+@RequestMapping("test")
+public class TestDemo {
+    @Test
+    public void test() {
+        System.out.println("testest");
+    }
+}
